c78e15e9e693dd43990b3501675eddd420675805
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2021-09-28 13:16:37 -0300

    initial

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreApi.Controllers
+{
+  [ApiController]
+  [Route("[controller]/[action]", Name = "[controller]_[action]")]
+  public class WeatherForecastController : ControllerBase
+  {
+    private static readonly string[] Summaries = new[]
+    {
+      "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Index()
+    {
+      var rng = new Random();
+      return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+      {
+        Date = DateTime.Now.AddDays(index),
+        TemperatureC = rng.Next(-20, 55),
+        Summary = Summaries[rng.Next(Summaries.Length)]
+      })
+      .ToArray();
+    }
+
+    [HttpGet]
+    public Dictionary<String, int> Details(int id)
+    {
+      return new Dictionary<string, int>{
+         {"a", 2},
+      };
+    }
+  }
+}
